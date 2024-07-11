83446a35cec7163864e225c577a32e84fb89dd9e
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-07-11 10:52:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-
-import './App.css';
-import CibilScoreGuide from './components/CibilScoreGuide/CibilScoreGuide';
-import CreditHealthReport from './components/CreditHealthReport/CreditHealthReport';
-import CreditInfoSection from './components/CreditInfo/CreditInfo';
-import CreditJourney from './components/CreditJourney/CreditJourney';
-import JoinCredit from './components/JoinCredit/JoinCredit';
+import "./App.css";
+import CibilScoreGuide from "./components/CibilScoreGuide/CibilScoreGuide";
+import CreditHealthReport from "./components/CreditHealthReport/CreditHealthReport";
+import CreditInfoSection from "./components/CreditInfo/CreditInfo";
+import CreditJourney from "./components/CreditJourney/CreditJourney";
+import JoinCredit from "./components/JoinCredit/JoinCredit";
+import Card from "./components/Plans/card";
 
 function App() {
   return (
     <div className="App">
-     <CreditJourney/>
-     <br/>
-     <JoinCredit/>
-     
-     <CreditInfoSection/>
-     <br/>
-     <CibilScoreGuide/>
-     <br/>
-     <CreditHealthReport/>
+      <CreditJourney />
+      <br />
+      <JoinCredit />
+      <CreditInfoSection />
+      <br />
+      <CibilScoreGuide />
+      <br />
+      <CreditHealthReport />
+      <br />
+      <Card />
     </div>
-   
-
-
   );
 }
 
---
+++ src/components/Plans/card.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./card.css";
+
+// const CheckmarkIcon = () => (
+//   <svg
+//     className="icon"
+//     viewBox="0 0 16 16"
+//     fill="currentColor"
+//     xmlns="http://www.w3.org/2000/svg"
+//   >
+//     <path
+//       fillRule="evenodd"
+//       d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 111.06-1.06l2.75 2.75 6.72-6.72a.75.75 0 011.06 0z"
+//     />
+//   </svg>
+// );
+
+const Card = () => {
+  return (
+    <div className="plans-container">
+      <h2 className="plans-title">Plans & Pricing</h2>
+      <div className="plans">
+        <div className="plan-card">
+          <h3 className="plan-type">Basic</h3>
+          <p className="plan-price">₹99</p>
+          <hr className="divider" />
+          <ul className="plan-features">
+            <li>Everything in Basic Plus</li>
+            <li>Comprehensive overview of your credit history.</li>
+            <li>Understanding your credit score and factors affecting it.</li>
+            <li>6 Month RM support.</li>
+          </ul>
+          <button className="plan-button">Get Started</button>
+        </div>
+        <div className="plan-card">
+          <h3 className="plan-type">Premium</h3>
+          <p className="plan-price">₹1,369</p>
+          <hr className="divider" />
+          <ul className="plan-features">
+            <li>Everything in Premium Plan</li>
+            <li>
+              Analysis of your credit report, including positive & negative
+              factors.
+            </li>
+            <li>Personalized action plan to improve your credit score.</li>
+            <li>6 Month RM support.</li>
+          </ul>
+          <button className="plan-button">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
